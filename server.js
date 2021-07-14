6bebacfaadbe4fd73e6173911f4f29a24f13adea
Jared Donovan <jdono100@gmail.com>
2021-07-13 22:39:17 -0500

    create models and file structure

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 86400,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({ db: sequelize })
